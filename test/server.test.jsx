f47b8290abc1e1c82007ea75027fa3a57fbc2150
Vania Gomes - Let's Coding <102826843+gomesvania@users.noreply.github.com>
2023-08-01 17:55:15 -0300

    Delete server.test.jsx

--- test/server.test.jsx
+++
@@ -1,8 +0,0 @@
-const superTest = require('supertest');
-
-const request = superTest('http://localhost:3001');
-test('Deve responder na porta 3001', () => {
-  // Acessar a url http://localhost::3001
-  return request.get('/').then(res => expect(res.status).toBe(200));
-  //verificar se a resposta foi o status 200
-});
